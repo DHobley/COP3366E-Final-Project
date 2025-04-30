e016eb913b3f8a8c4e50c94b788c0f2a5007dbbe
DHobley <144146937+DHobley@users.noreply.github.com>
2025-04-30 19:56:36 -0400

    Create MusicParticipant.cs

---
+++ MusicParticipant.cs
@@ -0,0 +1,15 @@
+public class MusicParticipant : Participant
+{
+    public string InstrumentType { get; set; }
+    public int NumberOfBandMembers { get; set; }
+    public int SetupTimeRequired { get; set; }
+
+    public MusicParticipant(string name, string contactInfo, double feePaid, bool isValid,
+                            string instrumentType, int bandMembers, int setupTime)
+        : base(name, "M", contactInfo, feePaid, isValid)
+    {
+        InstrumentType = instrumentType;
+        NumberOfBandMembers = bandMembers;
+        SetupTimeRequired = setupTime;
+    }
+}
