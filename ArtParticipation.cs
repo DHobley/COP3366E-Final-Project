ed22765c1a2610bb3704248217c158d8ccf6bf3f
DHobley <144146937+DHobley@users.noreply.github.com>
2025-05-01 16:23:19 -0400

    Update ArtParticipation.cs

--- ArtParticipation.cs
+++ ArtParticipation.cs
@@ -1,7 +1,7 @@
 public class ArtParticipant : Participant
 {
-    public string ArtMedium { get; set; }
-    public string DisplayDimensions { get; set; }
+    public string ArtMedium { get; set; } // "oil painting"
+    public string DisplayDimensions { get; set; } // "4ft x 6ft"
 
     public ArtParticipant(string name, string contactInfo, double feePaid, bool isValid,
                           string medium, string dimensions)
