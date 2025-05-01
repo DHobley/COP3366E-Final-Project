fe867233b88be020b68210162ac959911bc9385e
DHobley <144146937+DHobley@users.noreply.github.com>
2025-05-01 16:05:05 -0400

    Update MusicParticipant.cs

--- MusicParticipant.cs
+++ MusicParticipant.cs
@@ -2,7 +2,7 @@ public class MusicParticipant : Participant
 {
     public string InstrumentType { get; set; }
     public int NumberOfBandMembers { get; set; }
-    public int SetupTimeRequired { get; set; }
+    public int SetupTimeRequired { get; set; } // in minutes
 
     public MusicParticipant(string name, string contactInfo, double feePaid, bool isValid,
                             string instrumentType, int bandMembers, int setupTime)
