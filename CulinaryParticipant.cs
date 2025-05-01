93edf9b79179ef3823eee70fc2a8ce58cd693ade
DHobley <144146937+DHobley@users.noreply.github.com>
2025-05-01 16:24:34 -0400

    Update CulinaryParticipant.cs

--- CulinaryParticipant.cs
+++ CulinaryParticipant.cs
@@ -1,7 +1,7 @@
 public class CulinaryParticipant : Participant
 {
-    public string CuisineType { get; set; }
-    public int SampleServingSizes { get; set; }
+    public string CuisineType { get; set; } // "Caribbean street food"
+    public int SampleServingSizes { get; set; } // 50 taste-tests
 
     public CulinaryParticipant(string name, string contactInfo, double feePaid, bool isValid,
                                string cuisineType, int servings)
